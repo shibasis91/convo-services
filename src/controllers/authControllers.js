b9cb574b7249bcb54d0a16800e6b055db8343f51
Shibasis Roy <shibasis91@gmail.com>
2024-02-03 19:10:44 +0530

    Move app logic to controllers

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,44 @@
+const User = require("../models/Users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const registerUsers = async (req, res) => {
+  try {
+    const { username, phone, pin } = req.body;
+    const newUser = new User({ username, phone, pin });
+    const savedUser = await newUser.save();
+    return res
+      .status(201)
+      .json({ message: "User saved successfully", user: savedUser });
+  } catch (err) {
+    if (err.code === 11000 && err.keyPattern.phone && err.keyValue.phone) {
+      return res.status(400).json({
+        error: "This phone number is already in use",
+        number: err.keyValue.phone,
+      });
+    } else {
+      return res.status(500).json({ error: err.message });
+    }
+  }
+};
+
+const loginUsers = async (req, res) => {
+  try {
+    const { phone, pin } = req.body;
+    const user = await User.findOne({ phone });
+    if (!user || !(await bcrypt.compare(pin, user.pin))) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const SECRET_KEY = process.env.SECRET_KEY;
+    const authToken = jwt.sign({ userId: user._id }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    return res.json({ message: "Login successful", authToken, user });
+  } catch (err) {
+    if (err) console.log("Error: ", err);
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = registerUsers;
+module.exports = loginUsers;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,48 +1,13 @@
 const express = require("express");
-const User = require("../models/Users");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const registerUsers = require("../controllers/authControllers");
+const loginUsers = require("../controllers/authControllers");
 
 const authRouter = express.Router();
 
 //Register user
-authRouter.post("/signup", async (req, res) => {
-  try {
-    const { username, phone, pin } = req.body;
-    const newUser = new User({ username, phone, pin });
-    const savedUser = await newUser.save();
-    return res
-      .status(201)
-      .json({ message: "User saved successfully", user: savedUser });
-  } catch (err) {
-    if (err.code === 11000 && err.keyPattern.phone && err.keyValue.phone) {
-      return res.status(400).json({
-        error: "This phone number is already in use",
-        number: err.keyValue.phone,
-      });
-    } else {
-      return res.status(500).json({ error: err.message });
-    }
-  }
-});
+authRouter.post("/signup", registerUsers);
 
 //Login user
-authRouter.post("/login", async (req, res) => {
-  try {
-    const { phone, pin } = req.body;
-    const user = await User.findOne({ phone });
-    if (!user || !(await bcrypt.compare(pin, user.pin))) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-    const SECRET_KEY = process.env.SECRET_KEY;
-    const authToken = jwt.sign({ userId: user._id }, SECRET_KEY, {
-      expiresIn: "1h",
-    });
-    return res.json({ message: "Login successful", authToken, user });
-  } catch (err) {
-    if (err) console.log("Error: ", err);
-    return res.status(500).json({ error: err.message });
-  }
-});
+authRouter.post("/login", loginUsers);
 
 module.exports = authRouter;
